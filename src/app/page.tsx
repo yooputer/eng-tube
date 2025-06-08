b3394c4f56fe491d2a4853c226c2b86c77934e51
김유진 <yoojin027@mteletec.com>
2025-06-09 08:52:13 +0900

    make a logo div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
       <div>
-          <p className="font-bold text-rose-500 text-7xl">
-              test
-          </p>
-
-          <Button variant="destructive">
-              button
-          </Button>
+          <div>
+              <Image src={'/logo.svg'} height={50} width={50} alt={'Logo'}/>
+              <p className="text-xl font-semibold tracking-tight">EngTube</p>
+          </div>
       </div>
   )
 }
