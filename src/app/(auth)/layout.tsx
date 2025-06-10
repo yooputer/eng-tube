bae54032c3a822c6c2a50dcca0e8bdac1a7b4f54
김유진 <yoojin027@mteletec.com>
2025-06-10 09:22:12 +0900

    [Auth] create auth pages

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface LayoutProps {
+    children : React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
