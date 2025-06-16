43f3407bd54fc3ab06bb6d66f3c3865b05695fb4
김유진 <yoojin027@mteletec.com>
2025-06-17 08:56:56 +0900

    [Category] 카테고리 스키마 추가

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,3 +8,11 @@ export const users = pgTable("users", {
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 }, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)])
+
+export const categories = pgTable("categories", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull().unique(),
+    description: text("description"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+}, (t) => [uniqueIndex("name_idx").on(t.name)]);
