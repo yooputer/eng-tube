154c7eb2b19eab3c02f12a897905d2743e344abd
김유진 <yoojin027@mteletec.com>
2025-06-17 09:04:35 +0900

    refact: 테스트용 코드 제거

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,14 @@
 import {HydrateClient, trpc} from "@/trpc/server";
-import {PageClient} from "@/app/(home)/client";
 import {Suspense} from "react";
 import {ErrorBoundary} from "react-error-boundary";
 
 export default async function Home() {
-    void trpc.hello.prefetch();
 
   return (
       <HydrateClient>
           <Suspense fallback={<p>Loading...</p>}>
               <ErrorBoundary fallback={<p>Error....</p>}>
-                  <PageClient/>
+
               </ErrorBoundary>
           </Suspense>
       </HydrateClient>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,14 +1,6 @@
-import { z } from 'zod';
-import {baseProcedure, createTRPCRouter, protectedProcedure} from '../init';
+import { createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
-    hello: protectedProcedure
-        .query((opts) => {
-            const userName = opts.ctx.user.name;
 
-            return {
-                greeting: `hello ${userName}`,
-            };
-        }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
