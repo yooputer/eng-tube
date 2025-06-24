1b4d027bbf288d1271b2f2ef098f6d9564555a14
김유진 <yoojin027@mteletec.com>
2025-06-24 10:26:00 +0900

    feat: [Studio] studioRouter 생성, getMany 프로시져 구현

---
+++ src/modules/studio/server/procedures.ts
@@ -0,0 +1,51 @@
+import {createTRPCRouter, protectedProcedure} from "@/trpc/init";
+import {db} from "@/db";
+import {videos} from "@/db/schema";
+import {z} from "zod";
+import {and, desc, eq, lt, or} from "drizzle-orm";
+
+export const studioRouter = createTRPCRouter({
+    getMany: protectedProcedure
+        .input(
+            z.object({
+                cursor: z.object({
+                    id: z.string().uuid(),
+                    updatedAt: z.date(),
+                }).nullish(),
+                limit: z.number().min(1).max(100),
+            })
+        )
+        .query( async ({ ctx, input }) => {
+            const { cursor, limit } = input;
+            const { id: userId } = ctx.user;
+
+            const data = await db
+                .select()
+                .from(videos)
+                .where(and(
+                    eq(videos.userId, userId),
+                    cursor ? or(
+                        lt(videos.updatedAt, cursor.updatedAt),
+                        and(
+                            eq(videos.updatedAt, cursor.updatedAt),
+                            lt(videos.id, cursor.id)
+                        ))
+                        : undefined,
+                ))
+                .orderBy(desc(videos.updatedAt), desc(videos.id))
+                .limit(limit + 1);
+
+            // 만약 조회한 비디오의 개수가 (limit + 1)과 같으면 조회할 데이터가 있다는 뜻.
+            // 마지막 아이템은 지우고 반환
+            const hasMore = data.length > limit;
+            const items = hasMore ? data.slice(0, -1) : data;
+
+            const lastItem = items[items.length - 1];
+            const nextCursor = hasMore ? { id: lastItem.id, updatedAt: lastItem.updatedAt } : null;
+
+            return {
+                items,
+                nextCursor
+            };
+        }),
+})
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
 import { createTRPCRouter } from '../init';
 import {categoriesRouter} from "@/modules/categories/server/procedures";
+import {studioRouter} from "@/modules/studio/server/procedures";
 import {videosRouter} from "@/modules/videos/server/procedures";
 
 export const appRouter = createTRPCRouter({
     categories:categoriesRouter,
+    studio: studioRouter,
     videos: videosRouter,
 });
 // export type definition of API
