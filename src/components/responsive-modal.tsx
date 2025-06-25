5f5e689fa97a6ea3a98aa79ffaab9c1c60f326eb
김유진 <yoojin027@mteletec.com>
2025-06-26 08:50:29 +0900

    feat: ResponsiveModal 컴포넌트 구현
    - 모바일인경우 Drawer, 모바일이 아닌 경우 Dialog

---
+++ src/components/responsive-modal.tsx
@@ -0,0 +1,49 @@
+import {useIsMobile} from "@/hooks/use-mobile";
+import {Drawer, DrawerContent, DrawerHeader, DrawerTitle} from "@/components/ui/drawer";
+import {Dialog, DialogContent, DialogHeader, DialogTitle} from "@/components/ui/dialog";
+
+interface ResponsiveModalProps {
+    children: React.ReactNode;
+    open: boolean;
+    title: string;
+    onOpenChange: (open: boolean) => void;
+}
+
+export const ResponsiveModal = ({
+    children,
+    open,
+    title,
+    onOpenChange,
+}: ResponsiveModalProps) => {
+    const isMobile = useIsMobile();
+
+    if (isMobile) {
+        return (
+            <Drawer open={open} onOpenChange={onOpenChange}>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>
+                            {title}
+                        </DrawerTitle>
+                    </DrawerHeader>
+
+                    {children}
+                </DrawerContent>
+            </Drawer>
+        )
+    }
+
+    return (
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        {title}
+                    </DialogTitle>
+                </DialogHeader>
+
+                {children}
+            </DialogContent>
+        </Dialog>
+    );
+};
