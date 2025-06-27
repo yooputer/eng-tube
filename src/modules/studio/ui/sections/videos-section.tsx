70e53b7e5f0a75498f62849ce8ae1daabde2d4c8
김유진 <yoojin027@mteletec.com>
2025-06-27 10:44:13 +0900

    feat: [Studio] VideosSection에 썸네일 추가, Visibility/Status/Date 항목 수정

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -7,6 +7,10 @@ import {ErrorBoundary} from "react-error-boundary";
 import {InfiniteScroll} from "@/components/infinite-scroll";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import Link from "next/link";
+import {VideoThumbnail} from "@/modules/videos/ui/components/video-thumbnail";
+import {snakeCaseToTitle} from "@/lib/utils";
+import {format} from "date-fns";
+import {Globe2Icon, LockIcon} from "lucide-react";
 
 export const VideosSection = () => {
     return (
@@ -45,13 +49,38 @@ const VideosSectionSuspense = () => {
                             .map((video) => (
                                 <TableRow key={video.id} >
                                     <TableCell className="pl-6 w-[510px]">
-                                        <Link href={`/studio/videos/${video.id}`}>
-                                            {video.title}
-                                        </Link>
+                                        <div className="flex items-center gap-4">
+                                            <div className="relative aspect-video w-36 shrink-0">
+                                                <VideoThumbnail
+                                                    title={video.title}
+                                                    imageUrl={video.thumbnailUrl}
+                                                    previewUrl={video.previewUrl}
+                                                    duration={video.duration}
+                                                />
+                                            </div>
+                                            <div className="flex flex-col overflow-hidden gap-1">
+                                                <span className="text-sm line-clamp-1">{video.title}</span>
+                                                <span className="text-xs text-muted-foreground line-clamp-1">{video.description || "no description"}</span>
+                                            </div>
+                                        </div>
                                     </TableCell>
-                                    <TableCell>Visibility</TableCell>
-                                    <TableCell>{ video.muxStatus }</TableCell>
-                                    <TableCell>{video.createdAt.toISOString()}</TableCell>
+                                    <TableCell>
+                                        <div className="flex items-center">
+                                            { video.visibility === 'public'?
+                                                (<Globe2Icon className="size-4 mr-2" />)
+                                                : (<LockIcon className="size-4 mr-2" />)
+                                            }
+                                            { snakeCaseToTitle(video.visibility) }
+                                        </div>
+                                    </TableCell>
+                                    <TableCell>
+                                        <div className="flex items-center">
+                                            { snakeCaseToTitle(video.muxStatus || 'error') }
+                                        </div>
+                                    </TableCell>
+                                    <TableCell className="text-sm truncate">
+                                        { format(new Date(video.createdAt), "d MMM yyyy")}
+                                    </TableCell>
                                     <TableCell className="text-right">0</TableCell>
                                     <TableCell className="text-right">0</TableCell>
                                     <TableCell className="text-right pr-6">0</TableCell>
