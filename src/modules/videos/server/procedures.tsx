c510c90b8982e43f32bd35b59c05a86de7a20697
김유진 <yoojin027@mteletec.com>
2025-07-01 08:25:05 +0900

    feat: [Video] update, remove API 구현

--- src/modules/videos/server/procedures.tsx
+++ src/modules/videos/server/procedures.tsx
@@ -1,9 +1,64 @@
+import {and, eq } from "drizzle-orm";
 import {createTRPCRouter, protectedProcedure} from "@/trpc/init";
 import {db} from "@/db";
-import {videos} from "@/db/schema";
+import {videos, videoUpdateSchema} from "@/db/schema";
 import {mux} from "@/lib/mux";
+import {TRPCError} from "@trpc/server";
+import {z} from "zod";
 
 export const videosRouter = createTRPCRouter({
+    remove: protectedProcedure
+        .input(z.object({ id: z.string().uuid() }))
+        .mutation(async ({ ctx, input }) => {
+            const { id: userId } = ctx.user;
+
+            const [removedVideo] = await db
+                .delete(videos)
+                .where(and(
+                    eq(videos.id, input.id),
+                    eq(videos.userId, userId)
+                ))
+                .returning();
+
+            if (!removedVideo) {
+                throw new TRPCError({ code: "NOT_FOUND" });
+            }
+
+            return removedVideo;
+        }),
+    update: protectedProcedure
+        .input(videoUpdateSchema)
+        .mutation(async ({ctx, input}) => {
+            const {id: userId} = ctx.user;
+            const {id} = input;
+
+            if (!id) {
+                throw new TRPCError({code: "BAD_REQUEST"});
+            }
+
+            const [updatedVideo] = await db
+                .update(videos)
+                .set({
+                    title: input.title,
+                    description: input.description,
+                    categoryId: input.categoryId,
+                    visibility: input.visibility,
+                    updatedAt: new Date(),
+                })
+                .where(
+                    and(
+                        eq(videos.id, id),
+                        eq(videos.userId, userId),
+                    )
+                )
+                .returning();
+
+            if (!updatedVideo) {
+                throw new TRPCError({ code: "NOT_FOUND" });
+            }
+
+            return updatedVideo;
+        }),
     create: protectedProcedure.mutation(async ({ ctx }) => {
         const {id: userId} = ctx.user;
         const upload = await mux.video.uploads.create({
