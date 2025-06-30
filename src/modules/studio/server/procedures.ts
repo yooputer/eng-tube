fc75a75edc375bf40a9776400261afb75ec3ae24
김유진 <yoojin027@mteletec.com>
2025-07-01 08:24:46 +0900

    feat: [Studio] getOne API 구현

--- src/modules/studio/server/procedures.ts
+++ src/modules/studio/server/procedures.ts
@@ -3,8 +3,35 @@ import {db} from "@/db";
 import {videos} from "@/db/schema";
 import {z} from "zod";
 import {and, desc, eq, lt, or} from "drizzle-orm";
+import {TRPCError} from "@trpc/server";
 
 export const studioRouter = createTRPCRouter({
+    getOne: protectedProcedure
+        .input(
+            z.object({
+                id: z.string().uuid()
+            })
+        )
+        .query( async ({ctx, input}) => {
+            const {id: userId} = ctx.user;
+            const {id} = input;
+
+            const [video] = await db
+                .select()
+                .from(videos)
+                .where(
+                    and(
+                        eq(videos.id, id),
+                        eq(videos.userId, userId)
+                    )
+                )
+
+            if(!video){
+                throw new TRPCError({ code: "NOT_FOUND"});
+            }
+
+            return video;
+        }),
     getMany: protectedProcedure
         .input(
             z.object({
