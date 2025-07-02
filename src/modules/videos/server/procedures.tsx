540ec57c7fb6e930a9bea9784fd274b3ab71e640
김유진 <yoojin027@mteletec.com>
2025-07-02 10:45:52 +0900

    feat: [Studio] AI 제목/설명 생성 프로시져 구현

--- src/modules/videos/server/procedures.tsx
+++ src/modules/videos/server/procedures.tsx
@@ -6,8 +6,35 @@ import {mux} from "@/lib/mux";
 import {TRPCError} from "@trpc/server";
 import {z} from "zod";
 import {UTApi} from "uploadthing/server";
+import { workflow } from '@/lib/workflow';
 
 export const videosRouter = createTRPCRouter({
+    generateTitle: protectedProcedure
+        .input(z.object({ id: z.string().uuid() }))
+        .mutation(async ({ ctx, input }) => {
+            const {id: userId} = ctx.user;
+
+            const { workflowRunId } = await workflow.trigger({
+                url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/title`,
+                body: {userId, videoId: input.id, },
+                retries: 3,
+            });
+
+            return workflowRunId;
+        }),
+    generateDescription: protectedProcedure
+        .input(z.object({ id: z.string().uuid() }))
+        .mutation(async ({ ctx, input }) => {
+            const {id: userId} = ctx.user;
+
+            const { workflowRunId } = await workflow.trigger({
+                url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/description`,
+                body: {userId, videoId: input.id, },
+                retries: 3,
+            });
+
+            return workflowRunId;
+        }),
     restoreThumbnail: protectedProcedure
         .input(z.object({ id: z.string().uuid() }))
         .mutation(async ({ ctx, input }) => {
