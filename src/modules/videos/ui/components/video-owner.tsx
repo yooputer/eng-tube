f25911771aa2cf3ff245dca9272cc3999ff9aa87
김유진 <yoojin027@mteletec.com>
2025-07-08 09:36:11 +0900

    feat: [Video] 구독 기능 구현

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -6,6 +6,7 @@ import {useAuth} from "@clerk/nextjs";
 import {Button} from "@/components/ui/button";
 import {SubscriptionButton} from "@/modules/subscriptions/ui/components/subscription-button";
 import {UserInfo} from "@/modules/users/ui/components/user-info";
+import {useSubscription} from "@/modules/subscriptions/hooks/use-subscription";
 
 interface VideoOwnerProps {
     user: VideoGetOneOutput["user"];
@@ -14,10 +15,11 @@ interface VideoOwnerProps {
 
 export const VideoOwner = ({user, videoId}: VideoOwnerProps) => {
     const { userId: clerkUserId, isLoaded } = useAuth();
-
-    if (!user) {
-        return (<></>);
-    }
+    const { isPending, onClick } = useSubscription({
+        userId: user.id,
+        isSubscribed: user.viewerSubscribed,
+        fromVideoId: videoId,
+    });
 
     return (
         <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3 min-w-0">
@@ -28,8 +30,7 @@ export const VideoOwner = ({user, videoId}: VideoOwnerProps) => {
                     <div className="flex flex-col gap-1 min-w-0">
                         <UserInfo size="lg" name={user.name}/>
                         <span className="text-sm text-muted-foreground line-clamp-1">
-                            {/* TODO: 구독자수 매핑 */}
-                            {0} subscribers
+                            {user.subscriberCount} subscribers
                         </span>
                     </div>
                 </div>
@@ -47,9 +48,9 @@ export const VideoOwner = ({user, videoId}: VideoOwnerProps) => {
                 </Button>
              ) : (
                  <SubscriptionButton
-                     onClick={() => {}}
-                     disabled={false}
-                     isSubscribed={false}
+                     onClick={onClick}
+                     disabled={isPending || !isLoaded}
+                     isSubscribed={user.viewerSubscribed}
                      className="flex-none"
                  />
             )}
