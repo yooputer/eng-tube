a739cb6878a374b0a7b05e689e0edca2b8462e06
김유진 <yoojin027@mteletec.com>
2025-07-08 09:33:13 +0900

    feat: [Subscription] create, remove 프로시져 구현

---
+++ src/modules/subscriptions/server/procedures.ts
@@ -0,0 +1,47 @@
+import {createTRPCRouter, protectedProcedure} from "@/trpc/init";
+import {z} from "zod";
+import {db} from "@/db";
+import {subscriptions} from "@/db/schema";
+import {and, eq} from "drizzle-orm";
+import {TRPCError} from "@trpc/server";
+
+export const subscriptionsRouter = createTRPCRouter({
+    create: protectedProcedure
+        .input(z.object({userId: z.string().uuid()}))
+        .mutation(async ({input, ctx}) => {
+            const {userId} = input;
+
+            if (userId === ctx.user.id) {
+                throw new TRPCError(({code: "BAD_REQUEST"}));
+            }
+
+            const [createdSubscription] = await db
+                .insert(subscriptions)
+                .values({
+                    viewerId: ctx.user.id,
+                    creatorId: userId,
+                })
+                .returning();
+
+            return createdSubscription;
+        }),
+    remove: protectedProcedure
+        .input(z.object({userId: z.string().uuid()}))
+        .mutation(async ({input, ctx}) => {
+            const {userId} = input;
+
+            if (userId === ctx.user.id) {
+                throw new TRPCError(({code: "BAD_REQUEST"}));
+            }
+
+            const [deletedSubscription] = await db
+                .delete(subscriptions)
+                .where(and(
+                    eq(subscriptions.viewerId, ctx.user.id),
+                    eq(subscriptions.creatorId, userId),
+                ))
+                .returning();
+
+            return deletedSubscription;
+        }),
+})
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,6 +4,7 @@ import {studioRouter} from "@/modules/studio/server/procedures";
 import {videosRouter} from "@/modules/videos/server/procedures";
 import {videoViewsRouter} from "@/modules/video-views/server/procedures";
 import {videoReactionsRouter} from "@/modules/video-reactions/server/procedures";
+import {subscriptionsRouter} from "@/modules/subscriptions/server/procedures";
 
 export const appRouter = createTRPCRouter({
     categories:categoriesRouter,
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
     videos: videosRouter,
     videoViews: videoViewsRouter,
     videoReactions: videoReactionsRouter,
+    subscriptions: subscriptionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
