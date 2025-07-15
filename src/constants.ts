fe3f8d26822b551477caac18882fb1da00da84ca
김유진 <yoojin027@mteletec.com>
2025-07-16 08:53:36 +0900

    fix: APP_URL 할당문 수정

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_LIMIT = 5;
-export const APP_URL: string = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
+export const APP_URL: string = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : (process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000');
