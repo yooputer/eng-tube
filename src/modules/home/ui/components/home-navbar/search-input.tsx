5206ecc67256edcf406d98d2ae23bab8bba5ee6a
김유진 <yoojin027@mteletec.com>
2025-07-16 08:51:15 +0900

    fix: [Search] 검색시 기존 query, categoryId 유지되도록 수정

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -2,13 +2,16 @@
 
 import {SearchIcon, XIcon} from "lucide-react";
 import {useState} from "react";
-import {useRouter} from "next/navigation";
+import {useRouter, useSearchParams} from "next/navigation";
 import {APP_URL} from "@/constants";
 import {Button} from "@/components/ui/button";
 
 export const SearchInput = () => {
     const router = useRouter();
-    const [value, setValue] = useState("");
+    const searchParams = useSearchParams();
+    const query = searchParams.get("query") || '';
+    const categoryId = searchParams.get("categoryId");
+    const [value, setValue] = useState(query);
 
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -17,6 +20,10 @@ export const SearchInput = () => {
         const newQuery = value.trim();
 
         url.searchParams.set("query", encodeURIComponent(newQuery));
+
+        if (categoryId) {
+            url.searchParams.set("categoryId", encodeURIComponent(categoryId));
+        }
 
         if (newQuery === '') {
             url.searchParams.delete('query');
