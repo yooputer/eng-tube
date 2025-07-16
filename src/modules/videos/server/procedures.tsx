e923b0a0be5f86d0e67adac884bd43ad38965744
김유진 <yoojin027@mteletec.com>
2025-07-16 09:16:05 +0900

    feat: [Video] getMany 프로시져 구현

--- src/modules/videos/server/procedures.tsx
+++ src/modules/videos/server/procedures.tsx
@@ -1,4 +1,4 @@
-import {and, eq, getTableColumns, inArray, isNotNull} from "drizzle-orm";
+import {and, desc, eq, getTableColumns, inArray, isNotNull, lt, or} from "drizzle-orm";
 import {baseProcedure, createTRPCRouter, protectedProcedure} from "@/trpc/init";
 import {db} from "@/db";
 import {subscriptions, users, videoReactions, videos, videoUpdateSchema, videoViews} from "@/db/schema";
@@ -9,6 +9,57 @@ import {UTApi} from "uploadthing/server";
 import { workflow } from '@/lib/workflow';
 
 export const videosRouter = createTRPCRouter({
+    getMany: baseProcedure
+        .input(
+            z.object({
+                categoryId: z.string().uuid().nullish(),
+                cursor: z.object({
+                    id: z.string().uuid(),
+                    updatedAt: z.date(),
+                }).nullish(),
+                limit: z.number().min(1).max(100),
+            })
+        )
+        .query( async ({ input }) => {
+            const { categoryId, cursor, limit } = input;
+
+            const data = await db
+                .select({
+                    ...getTableColumns(videos),
+                    user: users,
+                    viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+                    likeCount: db.$count(videoReactions, and(eq(videoReactions.videoId, videos.id), eq(videoReactions.type, "like"))),
+                    dislikeCount: db.$count(videoReactions, and(eq(videoReactions.videoId, videos.id), eq(videoReactions.type, "dislike"))),
+                })
+                .from(videos)
+                .innerJoin(users, eq(videos.userId, users.id))
+                .where(and(
+                    eq(videos.visibility, "public"),
+                    categoryId ? eq(videos.categoryId, categoryId) : undefined,
+                    cursor ? or(
+                            lt(videos.updatedAt, cursor.updatedAt),
+                            and(
+                                eq(videos.updatedAt, cursor.updatedAt),
+                                lt(videos.id, cursor.id)
+                            ))
+                        : undefined,
+                ))
+                .orderBy(desc(videos.updatedAt), desc(videos.id))
+                .limit(limit + 1);
+
+            // 만약 조회한 비디오의 개수가 (limit + 1)과 같으면 조회할 데이터가 있다는 뜻.
+            // 마지막 아이템은 지우고 반환
+            const hasMore = data.length > limit;
+            const items = hasMore ? data.slice(0, -1) : data;
+
+            const lastItem = items[items.length - 1];
+            const nextCursor = hasMore ? { id: lastItem.id, updatedAt: lastItem.updatedAt } : null;
+
+            return {
+                items,
+                nextCursor
+            };
+        }),
     revalidate: protectedProcedure
         .input(z.object({
             id: z.string().uuid(),
