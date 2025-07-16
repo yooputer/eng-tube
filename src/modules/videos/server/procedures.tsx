4bc18578370bded9ae384cdcb651a9a4e5e88eaa
김유진 <yoojin027@mteletec.com>
2025-07-16 09:30:56 +0900

    feat: [Video] getManyTrending 프로시져 구현

--- src/modules/videos/server/procedures.tsx
+++ src/modules/videos/server/procedures.tsx
@@ -60,6 +60,60 @@ export const videosRouter = createTRPCRouter({
                 nextCursor
             };
         }),
+    getManyTrending: baseProcedure
+        .input(
+            z.object({
+                cursor: z.object({
+                    id: z.string().uuid(),
+                    viewCount: z.number(),
+                }).nullish(),
+                limit: z.number().min(1).max(100),
+            })
+        )
+        .query( async ({ input }) => {
+            const { cursor, limit } = input;
+
+            const viewCountSubquery = db.$count(
+                videoViews,
+                eq(videoViews.videoId, videos.id)
+            )
+
+            const data = await db
+                .select({
+                    ...getTableColumns(videos),
+                    user: users,
+                    viewCount: viewCountSubquery,
+                    likeCount: db.$count(videoReactions, and(eq(videoReactions.videoId, videos.id), eq(videoReactions.type, "like"))),
+                    dislikeCount: db.$count(videoReactions, and(eq(videoReactions.videoId, videos.id), eq(videoReactions.type, "dislike"))),
+                })
+                .from(videos)
+                .innerJoin(users, eq(videos.userId, users.id))
+                .where(and(
+                    eq(videos.visibility, "public"),
+                    cursor ? or(
+                            lt(viewCountSubquery, cursor.viewCount),
+                            and(
+                                eq(viewCountSubquery, cursor.viewCount),
+                                lt(videos.id, cursor.id)
+                            ))
+                        : undefined,
+                ))
+                .orderBy(desc(viewCountSubquery), desc(videos.id))
+                .limit(limit + 1);
+
+            // 만약 조회한 비디오의 개수가 (limit + 1)과 같으면 조회할 데이터가 있다는 뜻.
+            // 마지막 아이템은 지우고 반환
+            const hasMore = data.length > limit;
+            const items = hasMore ? data.slice(0, -1) : data;
+
+            const lastItem = items[items.length - 1];
+            const nextCursor = hasMore ? { id: lastItem.id, viewCount: lastItem.viewCount } : null;
+
+            return {
+                items,
+                nextCursor
+            };
+        }),
     revalidate: protectedProcedure
         .input(z.object({
             id: z.string().uuid(),
