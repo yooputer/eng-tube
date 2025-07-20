19aea4a83f871d55d73fc878851d150b0e399d0c
김유진 <yoojin027@mteletec.com>
2025-07-21 08:35:29 +0900

    fix: [DB] playlists/playlistVideos 스키마, relation 추가

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -38,7 +38,7 @@ export const userRelations = relations(users, ({ many }) => ({
     }),
     comments: many(comments),
     commentReactions: many(commentReactions),
-    // playlists: many(playlists),
+    playlists: many(playlists),
 }));
 
 export const subscriptions = pgTable("subscriptions", {
@@ -124,8 +124,8 @@ export const videoRelations = relations(videos, ({ one, many }) => ({
     }),
     views: many(videoViews),
     reactions: many(videoReactions),
-    // comments: many(comments),
-    // playlistVideos: many(playlistVideos),
+    comments: many(comments),
+    playlistVideos: many(playlistVideos),
 }));
 
 export const videoViews = pgTable("video_views", {
@@ -250,4 +250,45 @@ export const commentReactionRelations = relations(commentReactions, ({ one }) =>
         fields: [commentReactions.commentId],
         references: [comments.id],
     }),
+}));
+
+
+export const playlistVideos = pgTable("playlist_videos", {
+    playlistId: uuid("playlist_id").references(() => playlists.id, { onDelete: "cascade" }).notNull(),
+    videoId: uuid("video_id").references(() => videos.id, { onDelete: "cascade" }).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+}, (t) => [
+    primaryKey({
+        name: "playlist_videos_pk",
+        columns: [t.playlistId, t.videoId],
+    }),
+]);
+
+export const playlistVideoRelations = relations(playlistVideos, ({ one }) => ({
+    playlist: one(playlists, {
+        fields: [playlistVideos.playlistId],
+        references: [playlists.id],
+    }),
+    video: one(videos, {
+        fields: [playlistVideos.videoId],
+        references: [videos.id],
+    }),
+}));
+
+export const playlists = pgTable("playlists", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull(),
+    description: text("description"),
+    userId: uuid("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const playlistRelations = relations(playlists, ({ one, many }) => ({
+    user: one(users, {
+        fields: [playlists.userId],
+        references: [users.id],
+    }),
+    playlistVideos: many(playlistVideos),
 }));
