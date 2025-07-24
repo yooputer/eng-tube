bcc7b66e862b057147c10993040a35e28e6e8d4f
yooputer <yooputer@gmail.com>
2025-07-24 17:36:41 +0900

    fix: AuthButton에 프로필 버튼 추가

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,7 +1,7 @@
  "use client";
 
 import {Button} from "@/components/ui/button";
-import {ClapperboardIcon, UserCircleIcon} from "lucide-react";
+import {ClapperboardIcon, UserCircleIcon, UserIcon} from "lucide-react";
  import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
 
 export const AuthButton = () => {
@@ -12,7 +12,11 @@ export const AuthButton = () => {
             <SignedIn>
                 <UserButton>
                     <UserButton.MenuItems>
-                        {/* TODO: Add user profile menu button*/}
+                        <UserButton.Link
+                            href="/users/current"
+                            label="My Profile"
+                            labelIcon={<UserIcon className="size-4" />}
+                        />
                         <UserButton.Link
                             href="/studio"
                             label="Studio"
